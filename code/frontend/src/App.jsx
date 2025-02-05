aa1aa22bab7c8cda9639f77c7a976341631a53d1
gautambhetanabhotla <gautam.bhetanabhotla@students.iiit.ac.in>
2025-02-05 16:18:01 +0530

    Set up tailwind

--- code/frontend/src/App.jsx
+++ code/frontend/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="collapse bg-base-200">
+        <input type="radio" name="my-accordion-1" defaultChecked />
+        <div className="collapse-title text-xl font-medium">Click to open this one and close others</div>
+        <div className="collapse-content">
+          <p>hello</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="collapse bg-base-200">
+        <input type="radio" name="my-accordion-1" />
+        <div className="collapse-title text-xl font-medium">Click to open this one and close others</div>
+        <div className="collapse-content">
+          <p>hello</p>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="collapse bg-base-200">
+        <input type="radio" name="my-accordion-1" />
+        <div className="collapse-title text-xl font-medium">Click to open this one and close others</div>
+        <div className="collapse-content">
+          <p>hello</p>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ code/frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+import typography from "@tailwindcss/typography";
+import daisyui from "daisyui";
+
+export default {
+    content: [
+        './src/**/*.{js,ts,jsx,tsx}'
+    ],
+    plugins: [
+        typography,
+        daisyui,
+    ],
+};
