902643b429e4503adf1dd411b91413664ba39dc9
Nikhil8074 <nikhilrepala8074@gmail.com>
2025-03-03 16:00:07 +0530

    charts

--- code/frontend/src/project-view/project-page-charts.jsx
+++ code/frontend/src/project-view/project-page-charts.jsx
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Canvas from './project-page-charts/Canvas';
+import ChartModal from './project-page-charts/ChartModal';
 
-const Charts = () => {
+function Charts() {
+  const [charts, setCharts] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [editingChartId, setEditingChartId] = useState(null);
+
+  const handleAddChart = () => {
+    setEditingChartId(null);
+    setIsModalOpen(true);
+  };
+
+  const handleEditChart = (id) => {
+    setEditingChartId(id);
+    setIsModalOpen(true);
+  };
+
+  const handleSaveChart = (chartConfig) => {
+    if (editingChartId) {
+      setCharts(charts.map((chart) => (chart.id === editingChartId ? chartConfig : chart)));
+    } else {
+      setCharts([...charts, chartConfig]);
+    }
+  };
+
+  const editingChart = editingChartId ? charts.find((chart) => chart.id === editingChartId) : undefined;
+
   return (
-    <>
-    </>
+    <div className="app-container">
+      <Canvas 
+        charts={charts} 
+        onAddChart={handleAddChart} 
+        onEditChart={handleEditChart} 
+      />
+      
+      <ChartModal 
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onSave={handleSaveChart}
+        editingChart={editingChart}
+      />
+    </div>
   );
-};
+}
 
 export default Charts;
