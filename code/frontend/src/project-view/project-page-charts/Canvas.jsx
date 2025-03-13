800ce86082f3884cb471b0a102afa096cb4093ef
Nikhil8074 <nikhilrepala8074@gmail.com>
2025-03-13 18:27:11 +0530

    client side visualization completed

--- code/frontend/src/project-view/project-page-charts/Canvas.jsx
+++ code/frontend/src/project-view/project-page-charts/Canvas.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ChartComponent from './ChartComponent';
 import { PlusCircle } from 'lucide-react';
 
 const Canvas = ({ charts = [], onAddChart, onEditChart, onRemoveChart }) => {
   const chartsArray = Array.isArray(charts) ? charts : [];
-  
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const fetchUserDetails = async () => {
+      try {
+        const response = await fetch(`http://localhost:5000/api/user/getUser`, { credentials: 'include' });
+        const data = await response.json();
+        if (data.success) {
+          setUser(data.user);
+        } else {
+          alert(data.message);
+        }
+      } catch (error) {
+        console.error('Error fetching user details:', error);
+      }
+    };
+    
+    fetchUserDetails();
+  }, []);
   return (
     <div className="dashboard-container">
       <div className="dashboard-header">
         <h1 className="dashboard-title">Data Visualization Dashboard</h1>
+        { user?.role === 'admin' &&
         <button
           onClick={onAddChart}
           className="btn btn-primary btn-icon"
@@ -16,21 +34,32 @@ const Canvas = ({ charts = [], onAddChart, onEditChart, onRemoveChart }) => {
           <PlusCircle size={20} />
           <span>Add New Visualization</span>
         </button>
+        }
       </div>
 
       {charts.length === 0 ? (
-        <div className="empty-state">
-          <div className="empty-icon-container">
-            <PlusCircle size={48} className="empty-icon" />
+        user?.role === 'admin' ? (
+          <div className="empty-state">
+            { user?.role === 'admin' &&
+              <div className="empty-icon-container">
+                <PlusCircle size={48} className="empty-icon" />
+              </div>
+            }
+            { user?.role === 'admin' &&
+              <p className="empty-text">No Visualizations Yet</p>
+            }
+            { user?.role === 'admin' &&
+              <button
+                onClick={onAddChart}
+                className="btn btn-primary"
+              >
+                Add The First Visualization
+              </button>
+            }
           </div>
-          <p className="empty-text">No visualizations yet</p>
-          <button
-            onClick={onAddChart}
-            className="btn btn-primary"
-          >
-            Add The First Visualization
-          </button>
-        </div>
+        ) : (
+          <p className='empty-text'>No Visualizations Available</p>
+        )
       ) : (
         <div className="chart-grid">
           {charts.map((chart) => (
