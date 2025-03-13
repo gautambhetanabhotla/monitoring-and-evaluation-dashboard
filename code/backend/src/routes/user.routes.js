6a050d9c0e719b6ed6a4725b92320c65b4eb73a2
Viswas-Reddy-PallamReddy <155218470+Viswas-Reddy-PallamReddy@users.noreply.github.com>
2025-03-13 16:24:35 +0530

    Viswas (#69)
    * Added login redirections
    
    * Added Visualisation,User APIs
    
    * pull from main (#68)
    
    * Delete MoM/Week_7 directory
    
    * Add files via upload
    
    * Merging all the changes
    
    * Update visualisation.controller.js
    
    * Update models.js as per design document
    
    * merging update visualization frontend (#67)
    
    * Nikhil (#65)
    
    * Delete MoM/Week_7 directory
    
    * Add files via upload
    
    * Update models.js as per design document
    
    ---------
    
    Co-authored-by: saubhanik.chaudhuri <saubhanik.chaudhuri@research.iiit.ac.in>
    Co-authored-by: saubhanikchaudhuri <156495688+saubhanikchaudhuri@users.noreply.github.com>
    
    * frontend code for update visualization
    
    * pull from main by nikhil (#66)
    
    * Delete MoM/Week_7 directory
    
    * Add files via upload
    
    * Update models.js as per design document
    
    ---------
    
    Co-authored-by: saubhanik.chaudhuri <saubhanik.chaudhuri@research.iiit.ac.in>
    Co-authored-by: saubhanikchaudhuri <156495688+saubhanikchaudhuri@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: saubhanik.chaudhuri <saubhanik.chaudhuri@research.iiit.ac.in>
    Co-authored-by: saubhanikchaudhuri <156495688+saubhanikchaudhuri@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Nikhil_r_8074 <156401572+Nikhil8074@users.noreply.github.com>
    Co-authored-by: saubhanik.chaudhuri <saubhanik.chaudhuri@research.iiit.ac.in>
    Co-authored-by: saubhanikchaudhuri <156495688+saubhanikchaudhuri@users.noreply.github.com>
    
    * Update Visuaisation Added
    
    ---------
    
    Co-authored-by: Nikhil_r_8074 <156401572+Nikhil8074@users.noreply.github.com>
    Co-authored-by: saubhanik.chaudhuri <saubhanik.chaudhuri@research.iiit.ac.in>
    Co-authored-by: saubhanikchaudhuri <156495688+saubhanikchaudhuri@users.noreply.github.com>

--- code/backend/src/routes/user.routes.js
+++ code/backend/src/routes/user.routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { getClients, addUser } from '../controllers/users.controller.js';
+import { getClients, addUser, getUser,deleteUser} from '../controllers/users.controller.js';
 
 const userRouter = express.Router();
 
 userRouter.get('/clients', getClients);
 userRouter.post('/add', addUser);
+userRouter.get('/getDetails/:id', getUser);
+userRouter.delete('/delete/:id', deleteUser);
 
 export default userRouter;
--- code/backend/src/routes/visualisation.routes.js
+++ code/backend/src/routes/visualisation.routes.js
@@ -1,4 +1,7 @@
-import { createVisualisation, getVisualisationsByProject,deleteVisualisation} from "../controllers/visualisation.controller.js";
+import { createVisualisation,
+         getVisualisationsByProject,
+         deleteVisualisation,
+         updateVisualisation} from "../controllers/visualisation.controller.js";
 import express from 'express';
 
 const visualisationRouter = express.Router();
@@ -6,5 +9,6 @@ const visualisationRouter = express.Router();
 visualisationRouter.post('/save-visualisation', createVisualisation);
 visualisationRouter.get('/get-visualisations/:project_id', getVisualisationsByProject);
 visualisationRouter.delete('/delete-visualisation/:id', deleteVisualisation);
+visualisationRouter.put('/update-visualisation/:id', updateVisualisation);
 
 export default visualisationRouter;
